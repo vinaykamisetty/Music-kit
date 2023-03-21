9d5884dadb6f203a4db9ce5d5d1b237d928053d4
vinaykamisetty <126880082+vinaykamisetty@users.noreply.github.com>
2023-03-21 11:52:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,32 +15,32 @@ document.addEventListener("keydown", function (event) {
 
 function makeSound(key) {
   switch (key) {
-    case "v":
+    case "V":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
-    case "i":
+    case "I":
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
-    case "n":
+    case "N":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
-    case "a":
+    case "A":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
-    case "y":
+    case "Y":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
-    case "v":
+    case "V":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
-    case "k":
+    case "K":
       var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
